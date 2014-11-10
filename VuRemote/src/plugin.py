4c4e06415f0ebaaab33d912ed2cb29c53331a8b5
rossi2000 <rossi_2000@hotmail.com>
2014-11-10 22:55:26 +0000

    [VuRemote] add text support for openvixhd

--- VuRemote/src/plugin.py
+++ VuRemote/src/plugin.py
@@ -83,7 +83,7 @@ class RemoteControlCode(Screen,ConfigListScreen,RemoteControlCodeInit):
 		self.list = []
 		self.rcsctype = getConfigListEntry(_("Remote Control System Code"), config.plugins.remotecontrolcode.systemcode)
 		self.list.append( self.rcsctype )
-		if getImageDistro() in ("openvix", "openatv"):
+		if getImageDistro() in ("openvix", "openvixhd", "openatv"):
 			self.list.append(getConfigListEntry(_("Text support"), config.misc.remotecontrol_text_support))
 		self["config"].list = self.list
 		self["config"].l.setList(self.list)
